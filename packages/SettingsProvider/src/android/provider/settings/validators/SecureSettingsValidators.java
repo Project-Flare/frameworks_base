8de939f209746f840085535e1c25f079deb151fe
Jesse Chan <jc@lineageos.org>
2020-05-19 16:05:23 +0800

    SystemUI: Implement hide gestural navigation hint bar [1/3]
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Tue May 19 16:05:23 2020 +0800
    
        Implement hide gestural navigation hint bar [1/5]
    
        Change-Id: Ic6695dfe929db443f169d59f153e14a6de249590
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Tue Jun 2 13:59:52 2020 +0800
    
        SystemUI: Set no hint overlay to highest priority
    
        As overlays are also used to apply navigation mode, it is needed to set
        our customization overlay to highest priority to ensure it is applied.
    
        Change-Id: I41ca4b7ea99fea9f62d2e8ece5114f682e8e7fe9
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun May 15 11:17:03 2022 +0200
    
        SystemUI: Catch IllegalArgumentException in updateHint()
    
        Apparently this can fail for some very unlucky users?
    
        Change-Id: I3609746f9b577b91525fd64f586c2e79663f9ada
    
    Author: Dhina17 <dhinalogu@gmail.com>
    Date: Sun, 3 Mar 2024 11:46:29 +0000
    
        fixup! SystemUI: Implement hide gestural navigation hint bar
    
        Views should be touched only from main thread.
    
        Change-Id: Ib2e464eba593e870d120aea7181b3ad8f4229fc6
    
    Author: Alcatraz323 <alcatraz32323@gmail.com>
    Date: Wed, 20 Apr 2022 21:54:44 +0800
    
        NavigationBarInflaterView: fix concurrent unsafe variable leads to nohint overlay reload forever
    
        * This variable can be accessed by multiple trigger below
    
        com.android.systemui.navigationbar.NavigationModeController$2.onReceive(NavigationModeController.java:114)
        com.android.systemui.navigationbar.NavigationBarInflaterView.onTuningChanged(NavigationBarInflaterView.java:224)
    
        Change-Id: I7df878653821dd641923c401e7ff3749c86c4ce8
    
    Change-Id: Ic6695dfe929db443f169d59f153e14a6de249590
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -469,5 +469,6 @@ public class SecureSettingsValidators {
                 new InclusiveIntegerRangeValidator(0, 1));
         VALIDATORS.put(Secure.STATUS_BAR_CLOCK, new InclusiveIntegerRangeValidator(0, 2));
         VALIDATORS.put(Secure.TETHERING_ALLOW_VPN_UPSTREAMS, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.NAVIGATION_BAR_HINT, BOOLEAN_VALIDATOR);
     }
 }
